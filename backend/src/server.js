1ca99f3e1fd7013b362d1f4a9d5832dc1a6319ef
AK47 <anishwithcode@gmail.com>
2026-02-01 22:26:18 +0530

    edit ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,8 +24,7 @@ app.use("/api/messages" , messageRoutes);
 //make ready for deployment
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("*", (_,res) =>{
+    app.use((req,res) =>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     });
 }
